e322b4144a5680d8c73a4dc7cb547c912a841322
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-08 22:26:18 +0530

    views wip:

--- core/Application.php
+++ core/Application.php
@@ -2,9 +2,12 @@
 namespace app\core;
 class Application{
 
+
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
-    public function __construct(){
+    public function __construct($rootPath){
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
         
@@ -12,7 +15,7 @@ class Application{
 
 
     public function run(){
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
 ?>
--- core/Router.php
+++ core/Router.php
@@ -18,11 +18,35 @@ class Router{
     //    var_dump($this->routes);
        $callback = $this->routes[$method][$path] ?? false;
        if($callback === false){
-           echo 'Not Found';
-           exit;
+           return 'Not Found';
        }
-       echo call_user_func($callback);
+       if(is_string($callback)){
+            return $this->renderView($callback);
+       }
+       return call_user_func($callback);
        
     }
+
+
+
+    public function renderView($view){
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}',$viewContent,$layoutContent);
+    }
+
+
+    protected function layoutContent(){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
+
 }
 ?>

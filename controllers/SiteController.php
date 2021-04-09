e07901c1028e3852d97f80b6c555235111baffec
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-09 17:34:42 +0530

    POST data sanitize done

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
     class SiteController extends Controller{
 
 
@@ -14,7 +15,10 @@ use app\core\Controller;
         public   function contact(){
             return $this->render('contact');
         }
-        public   function handleContact(){
+        public   function handleContact(Request $request){
+
+            $body = $request->getBody();
+            var_dump($body);
             return 'handeling submited data';
         }
     }
